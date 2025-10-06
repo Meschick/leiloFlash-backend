82eb7469a51daa72a02b9bc5660d25d66d8ac2fa
Kawan Passos Meschick <kawanpassos5000@gmail.com>
2025-10-05 21:43:27 -0300

    Add project files.

---
+++ leiloFlash-backend/Models/CompradorClass.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.EntityFrameworkCore.Metadata.Conventions;
+using System.ComponentModel.DataAnnotations;
+
+namespace leiloFlash_backend.Models
+{
+    public class CompradorClass
+    {
+
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required( ErrorMessage = "O nome é obrigatório")]
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public DateTime DataCadastro { get; set; }
+        public string Cpf { get; set; }
+        public string Telefone { get; set; }
+
+    }
+}
