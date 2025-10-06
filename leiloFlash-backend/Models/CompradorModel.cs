f03cec0e3e817ce05ed40e67b735bf043985a4ca
Kawan Passos Meschick <kawanpassos5000@gmail.com>
2025-10-05 22:38:44 -0300

    feat: Implementa novas models

--- leiloFlash-backend/Models/CompradorModel.cs
+++ leiloFlash-backend/Models/CompradorModel.cs
@@ -4,19 +4,30 @@ using System.ComponentModel.DataAnnotations;
 
 namespace leiloFlash_backend.Models
 {
-    public class CompradorClass
+    public class CompradorModel
     {
-
-
         [Key]
         public int Id { get; set; }
 
         [Required( ErrorMessage = "O nome é obrigatório")]
         public string Nome { get; set; }
-        public string Email { get; set; }
+
+        [Required(ErrorMessage = "O email é obrigatório")]
+        public string Email { get; set; }           
         public DateTime DataCadastro { get; set; }
+
+        [Required(ErrorMessage = "O CPF é obrigatório")]
         public string Cpf { get; set; }
+
+        [Required(ErrorMessage = "O telefone é obrigatório")]
         public string Telefone { get; set; }
+
+        [Required]
+        public EnderecoModel Endereco { get; set; }
+
+        [Required]
+        public int EnderecoId { get; set; }
+
 
     }
 }
---
+++ leiloFlash-backend/Models/EnderecoModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace leiloFlash_backend.Models
+{
+    public class EnderecoModel
+    {
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "O logradouro é obrigatório")]
+        public string Logradouro { get; set; }
+
+        public string Numero { get; set; }  
+        public string Complemento { get; set; }
+
+
+        [Required(ErrorMessage = "O bairro é obrigatório")]
+        public string Bairro { get; set; }
+
+
+        [Required(ErrorMessage = "A cidade é obrigatória")]
+        public string Cidade { get; set; }
+
+    }
+}
