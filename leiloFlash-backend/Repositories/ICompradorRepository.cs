7eb735914cd4a0bfe64e20fd09a637c6a21dd9cb
Kawan Passos Meschick <kawanpassos5000@gmail.com>
2025-10-07 00:55:31 -0300

    feat: Implementa Repositories

---
+++ leiloFlash-backend/Repositories/ICompradorRepository.cs
@@ -0,0 +1,9 @@
+﻿using leiloFlash_backend.Models;
+
+namespace leiloFlash_backend.Repositories
+{
+    public interface ICompradorRepository
+    {
+        Task CreateComprador(CompradorModel comprador);
+    }
+}
