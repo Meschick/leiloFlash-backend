f204c17623252656e3ae2f482995d018039f7dd4
Kawan Passos Meschick <kawanpassos5000@gmail.com>
2025-10-07 00:54:16 -0300

    feat: Implementa arquivo de conexão e schema do banco

---
+++ leiloFlash-backend/Data/LeiloDbContext.cs
@@ -0,0 +1,25 @@
+﻿using leiloFlash_backend.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace leiloFlash_backend.Data
+{
+    public class LeiloDbContext: DbContext
+    {
+
+        public LeiloDbContext(DbContextOptions<LeiloDbContext> options) : base(options) { }
+
+
+
+        // Tabelas do banco de dados
+        DbSet<CompradorModel> Compradores { get; set; }
+        DbSet<EnderecoModel> Enderecos { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<CompradorModel>().ToTable("Comprador");
+            modelBuilder.Entity<EnderecoModel>().ToTable("Endereco");
+        }
+
+    }
+}
