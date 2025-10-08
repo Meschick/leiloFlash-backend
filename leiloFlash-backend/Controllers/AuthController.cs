44824d42a894f1223dbd2eea18dcd59230f2709d
Kawan Passos Meschick <kawanpassos5000@gmail.com>
2025-10-07 23:24:20 -0300

    feat: Novas Controllers

---
+++ leiloFlash-backend/Controllers/AuthController.cs
@@ -0,0 +1,30 @@
+﻿using leiloFlash_backend.DTO;
+using leiloFlash_backend.Services.Auth;
+using Microsoft.AspNetCore.Mvc;
+
+namespace leiloFlash_backend.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class AuthController : Controller
+    {
+
+        private readonly IAuthService _authService;
+
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] LoginDTO loginDto)
+        {
+
+            var token = await _authService.LoginAsync(loginDto.Email, loginDto.Senha);
+            if (token == null)
+                return Unauthorized("Email ou Senha inválidos");
+
+            return Ok(new { Token = token });
+        }
+    }
+}
---
+++ leiloFlash-backend/Controllers/CompradorController.cs
@@ -0,0 +1,26 @@
+﻿using leiloFlash_backend.DTO;
+using leiloFlash_backend.Models;
+using leiloFlash_backend.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace leiloFlash_backend.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class CompradorController : Controller
+    {
+        private readonly ICompradorService _compradorService;
+
+        public CompradorController(ICompradorService compradorService)
+        {
+            _compradorService = compradorService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PostComprador([FromBody] CompradorDTO compradorDto)
+        {
+            await _compradorService.CreateComprador(compradorDto);
+                return Ok("Comprador criado com sucesso!");
+        }
+    }
+}
